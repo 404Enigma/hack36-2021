5185c3efe3bf0e7393c3e3aa226ad905439e7389
Pratyush Jain <pratyush.jain2001@gmail.com>
2021-04-10 05:00:12 +0530

    patient list page

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,13 @@ const { checkCookie, savecookie } = require("../middleware/auth");
 router.get("/", (req, res) => {
   res.render("pages/home");
 });
+
 router.get("/treatment", (req, res) => {
   res.render("pages/treatment");
+});
+
+router.get("/patient_list", (req, res) => {
+  res.render("pages/patient_list");
 });
 
 router.get("/main", checkCookie, (req, res) => {
